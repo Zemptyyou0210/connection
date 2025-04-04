5d695067ab1e0dbd74d0bb993780de1f711e7317
Zemptyyou0210 <aa0937217428@gmail.com>
2025-04-04 11:45:29 +0800

    Update connection.py

--- connection.py
+++ connection.py
@@ -179,7 +179,6 @@ def create_drug_form(ward, drugs):
     for drug, limit in drugs.items():
         with st.expander(drug):
             drug_data = {}
-            reviewed = st.checkbox(f"✅ 已完成 {drug} 查核", key=f"{drug}_reviewed")
             complete = True 
             for col in COLUMNS:
                 if col == "現存量":
@@ -240,7 +239,7 @@ def create_drug_form(ward, drugs):
                 if not complete:
                     incomplete_drugs.append(drug)
                     
-
+                reviewed = st.checkbox(f"✅ 已完成 {drug} 查核", key=f"{drug}_reviewed")
                 data[drug] = drug_data
                 drug_data["已完成查核"] = reviewed
                 data[drug] = drug_data                    
