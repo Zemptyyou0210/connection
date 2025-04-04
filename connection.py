d89bd8c54e3ec29080a5334c70672a1201d91a18
Zemptyyou0210 <aa0937217428@gmail.com>
2025-04-04 12:06:24 +0800

    Update connection.py

--- connection.py
+++ connection.py
@@ -242,8 +242,8 @@ def create_drug_form(ward, drugs):
                 data[drug] = drug_data
                 st.markdown("---")
                     
-        reviewed = st.checkbox(f"✅ 已完成 {drug} 查核", key=f"{drug}_reviewed")
-        drug_data["已完成查核"] = reviewed
+            reviewed = st.checkbox(f"✅ 已完成 {drug} 查核", key=f"{drug}_reviewed")
+            drug_data["已完成查核"] = reviewed
         data[drug] = drug_data                    
         st.markdown("---")
 
