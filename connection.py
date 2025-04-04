b6898df6525a9a14487a20ca95c168710b27d4bd
Zemptyyou0210 <aa0937217428@gmail.com>
2025-04-04 11:48:32 +0800

    Update connection.py

--- connection.py
+++ connection.py
@@ -238,9 +238,10 @@ def create_drug_form(ward, drugs):
                     complete = False
                 if not complete:
                     incomplete_drugs.append(drug)
+                data[drug] = drug_data
+                st.markdown("---")
                     
             reviewed = st.checkbox(f"✅ 已完成 {drug} 查核", key=f"{drug}_reviewed")
-            data[drug] = drug_data
             drug_data["已完成查核"] = reviewed
             data[drug] = drug_data                    
             st.markdown("---")
